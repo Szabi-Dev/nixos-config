a7d548486273bcad43cd561d2c8753c6d5ca5043
Szabi-Dev <71439786+Szabi-Dev@users.noreply.github.com>
2024-07-06 21:24:24 +0200

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,10 +59,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-#  services.xserver.displayManager.gdm.enable = true;
-#  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.windowManager.bspwm.enable = true;
-  # Configure keymap in X11
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+ 
+# Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
