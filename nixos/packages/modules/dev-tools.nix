9bc38fb739968b65ae6e121e79aac0cb1f6d81ca
Szabidev <info@szabidev.com>
2024-07-07 13:02:30 +0200

    jetbrains idea and docker

--- nixos/packages/modules/dev-tools.nix
+++ nixos/packages/modules/dev-tools.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+	# misc
 	pkgs.wget
 	pkgs.git
 	pkgs.curl
-  ];
+	#idea
+ 	pkgs.jetbrains.idea-ultimate
+
+	# container
+	pkgs.docker
+ ];
  
 }
