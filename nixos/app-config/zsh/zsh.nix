925798a54fdf6628284844680a40031e08c50afa
Szabidev <info@szabidev.com>
2024-07-07 12:43:14 +0200

    zsh

--- nixos/app-config/zsh/zsh.nix
+++ nixos/app-config/zsh/zsh.nix
@@ -3,10 +3,14 @@
 programs.zsh = {
   enable = true;
   enableCompletion = true;
+  syntaxHighlighting.enable = true;
   
   oh-my-zsh = {
 	enable = true;
 	theme = "robbyrussell";
+  	plugins = [
+		"git"
+	];
   };
 
 
