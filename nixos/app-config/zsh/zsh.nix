8fe30e4918a503ebbfefaacc331f4992a18ea495
Szabidev <info@szabidev.com>
2024-07-07 12:29:25 +0200

    zsh

--- nixos/app-config/zsh/zsh.nix
+++ nixos/app-config/zsh/zsh.nix
@@ -3,7 +3,17 @@
 programs.zsh = {
   enable = true;
   enableCompletion = true;
+  enableAutosuggestions.enable = true;
+  syntaxHighlighting.enable = true;
 
+  oh-my-zsh = {
+  	enable = true;
+	theme = "robbyrussel"
+	plugins = [
+		"git"
+		"history"
+	];
+  };
   history = {
     size = 10000;
     path = "${config.xdg.dataHome}/zsh/history";
