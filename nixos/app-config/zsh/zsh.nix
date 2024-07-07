3931d9818726923919a19a738e3d2ef80b6f9059
Szabidev <info@szabidev.com>
2024-07-07 13:47:24 +0200

    zsh:

--- nixos/app-config/zsh/zsh.nix
+++ nixos/app-config/zsh/zsh.nix
@@ -21,4 +21,20 @@ programs.zsh = {
 
 };
 
+xdg.configFile."zsh/aliases/general".source = ./alias/general
+xdg.configFile."zsh/aliases/java".source = ./alias/java
+xdg.configFile."zsh/aliases/node".source = ./alias/node
+
+
+home.file.".zshrc".text = ''
+	# Custom ZSH Binds
+	bindkey '^ ' autosuggest-accept
+
+	# Load aliases and shortcuts if existent.
+	ZSH_ALIAS_HOME=$HOME/.config/zsh/aliases/
+	if [ -d "$ZSH_ALIAS_HOME" ]; then 
+    		for FILE in $ZSH_ALIAS_HOME/* ; do source $FILE ; done
+	fi
+''
+
 }
