a18c5cce8c464d4fba8c0a75ae29b500ba336097
Szabidev <info@szabidev.com>
2024-07-10 10:06:36 +0200

    vs code extensions for java

--- nixos/home-manager/vscode/vscode.nix
+++ nixos/home-manager/vscode/vscode.nix
@@ -5,6 +5,8 @@
 		package = pkgs.vscodium;
   		extensions = with pkgs.vscode-extensions; [
                   vscjava.vscode-java-pack
+                  vscjava.vscode-spring-initializr
+                  vscjava.vscode-spring-boot-dashboard
   		];
 };
 }
