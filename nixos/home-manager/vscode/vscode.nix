7db23c963ddf4e3ccb4ca923fbc6e42d53d5f76f
Szabidev <info@szabidev.com>
2024-07-10 09:42:42 +0200

    vscode

--- nixos/home-manager/vscode/vscode.nix
+++ nixos/home-manager/vscode/vscode.nix
@@ -3,12 +3,6 @@
 	programs.vscode = {
   		enable = true;
   		extensions = with pkgs.vscode-extensions; [
-			# general
-    			alefragnani.project-manager
-    			usernamehw.errorlens
-    			# angular
-    			esbenp.prettier-vscode
-    			Angular.ng-template
     			# appearence 
     			sainnhe.everforest
   		];
