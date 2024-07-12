098433b94f354903c8b5c653087771c66bfe190b
Szabidev <info@szabidev.com>
2024-07-13 00:00:42 +0200

    grub

--- nixos/settings/boot/grub.nix
+++ nixos/settings/boot/grub.nix
@@ -1,6 +1,6 @@
 { config, pkgs, input, ... }:
 {
 	boot.loader.grub.enable = true;
-        boot.loader.grub.device = "/dev/nvme0";
+        boot.loader.grub.device = "/dev/nvme0n1";
         boot.loader.grub.useOSProber = true;
 }
