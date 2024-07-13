e5b76cbce7e95a04428845baa0a78dc614d3d218
Szabidev <info@szabidev.com>
2024-07-13 09:15:35 +0200

    node versioning

--- nixos/packages/modules/dev-languages.nix
+++ nixos/packages/modules/dev-languages.nix
@@ -14,7 +14,7 @@
   environment.sessionVariables.JAVA_17_HOME="${pkgs.jdk17.home}";
   environment.sessionVariables.JAVA_HOME="${pkgs.jdk17.home}";  
 
-  environment.sessionVariables.NPM_20_HOME="${pkgs.nodejs_20.bin}";
-  environment.sessionVariables.NPM_22_HOME="${pkgs.nodejs_22.bin}";
+  environment.sessionVariables.NPM_20_HOME="${pkgs.nodejs_20}";
+  environment.sessionVariables.NPM_22_HOME="${pkgs.nodejs_22}";
 
 }
