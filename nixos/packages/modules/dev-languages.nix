dc14b3ffcbb85801d1de9606edac9e1642ebf818
Szabidev <info@szabidev.com>
2024-07-13 08:33:53 +0200

    node js 20

--- nixos/packages/modules/dev-languages.nix
+++ nixos/packages/modules/dev-languages.nix
@@ -5,11 +5,13 @@
 	      pkgs.jdk17	
 
         pkgs.python3
+
+        pkgs.nodejs_20
   ];
 
   environment.sessionVariables.JAVA_8_HOME="${pkgs.jdk8.home}";
   environment.sessionVariables.JAVA_17_HOME="${pkgs.jdk17.home}";
-
-
   environment.sessionVariables.JAVA_HOME="${pkgs.jdk17.home}";  
+
+  environment.sessionVariables.NPM_20_HOME="${pkgs.nodejs_20.home}"
 }
