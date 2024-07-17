ec5346db5f31a9dc0ec71d26ee1e2dbb4b999f49
Szabidev <info@szabidev.com>
2024-07-17 13:46:59 +0200

    bash linking script improved

--- nixos/scripts/modules/bashlinking.nix
+++ nixos/scripts/modules/bashlinking.nix
@@ -6,7 +6,7 @@
             standard_bash_path='/bin/bash'
             nix_bash_path='/bin/sh/'
 
-            if [ -e $standard_bash_path ] then
+            if [[ -e $standard_bash_path ]]; then
                 echo "bashlinking-script: $standard_bash_path already exists"
             else
                 ln -s "$nix_bash_path" "$standard_bash_path"
