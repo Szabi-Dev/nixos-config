f97612c5d45809dd394c3b8d7e684fb34967236b
Szabidev <info@szabidev.com>
2024-11-05 20:11:51 +0100

    kitty new theme

--- nixos/home-manager/kitty/kitty.nix
+++ nixos/home-manager/kitty/kitty.nix
@@ -4,5 +4,6 @@
  
   xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
   xdg.configFile."kitty/gruvbox.theme".source = ./gruvbox.theme;
+  xdg.configFile."kitty/gruvbox_material_dark.theme".source = ./gruvbox_material_dark.theme;
 
 }
