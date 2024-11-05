5a885b3755b236101869e9534abc688889ba9b53
Szabidev <info@szabidev.com>
2024-11-05 20:22:59 +0100

    kitty themes

--- nixos/home-manager/kitty/kitty.nix
+++ nixos/home-manager/kitty/kitty.nix
@@ -3,7 +3,11 @@
   home.packages = [ pkgs.kitty ];
  
   xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
-  xdg.configFile."kitty/gruvbox.theme".source = ./gruvbox.theme;
-  xdg.configFile."kitty/gruvbox_material_dark.theme".source = ./gruvbox_material_dark.theme;
-
+  
+  xdg.configFile."kitty/catpuccin_frappe.theme".source = ./themes/catpuccin_frappe.theme;
+  xdg.configFile."kitty/everforest.theme".source = ./themes/everforest.theme;
+  xdg.configFile."kitty/gruvbox_material_dark.theme".source = ./themes/gruvbox_material_dark.theme;
+  xdg.configFile."kitty/gruvbox.theme".source = ./themes/gruvbox.theme;
+  xdg.configFile."kitty/tokyo_night_storm.theme".source = ./themes/tokyo_night_storm.theme;
+  
 }
